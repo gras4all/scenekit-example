308b307c0aa48864b24e179e11df3d17c148af61
Andrey Grunenkov <60237240+gras4all@users.noreply.github.com>
2022-07-05 01:08:13 +0400

    Initial Commit

---
+++ scenekit-example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  scenekit-example
+//
+//  Created by Андрей Груненков on 05.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
